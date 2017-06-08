2d873ccc6389fbd2fb733e104e08f7eb6f87fee3
stormcat24 <stormcat24@stormcat.io>
2017-06-08 09:39:52 +0900

    copy .proto file recursively

---
+++ helper/io.go
@@ -0,0 +1,30 @@
+package helper
+
+import (
+	"os"
+	"strings"
+	"github.com/pkg/errors"
+	"io/ioutil"
+)
+
+func WriteFileWithDirectory(path string, data []byte, perm os.FileMode) error {
+
+	s := strings.Split(path, "/")
+
+	var dir string
+	if len(s) > 1 {
+		dir = strings.Join(s[0:len(s) -1], "/")
+	} else {
+		dir = path
+	}
+
+	if err := os.MkdirAll(dir, 0777); err != nil {
+		return errors.Wrapf(err, "create directory is failed. [%s]", dir)
+	}
+
+	if err := ioutil.WriteFile(path, data, perm); err != nil {
+		return errors.Wrapf(err, "write data to file is failed. [%s]", path)
+	}
+
+	return nil
+}
