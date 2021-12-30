2082be8b6cf0f60167351e5f9e1cfb6b066b3450
stormcat24 <stormcat24@stormcat.io>
2021-12-30 22:55:27 +0900

    Migrate pkg/errors to Go 1.13 errors

--- pkg/repository/git.go
+++ pkg/repository/git.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing"
-	"github.com/pkg/errors"
 
 	"github.com/stormcat24/protodep/pkg/auth"
 	"github.com/stormcat24/protodep/pkg/config"
@@ -63,7 +62,7 @@ func (r *github) Open() (*OpenedRepository, error) {
 
 		rep, err = git.PlainOpen(repopath)
 		if err != nil {
-			return nil, errors.Wrap(err, "open repository is failed")
+			return nil, fmt.Errorf("open repository: %w", err)
 		}
 		spinner.Stop()
 
@@ -76,7 +75,7 @@ func (r *github) Open() (*OpenedRepository, error) {
 
 		if err := rep.Fetch(fetchOpts); err != nil {
 			if err != git.NoErrAlreadyUpToDate {
-				return nil, errors.Wrap(err, "fetch repository is failed")
+				return nil, fmt.Errorf("fetch repository: %w", err)
 			}
 		}
 		spinner.Finish()
@@ -89,29 +88,29 @@ func (r *github) Open() (*OpenedRepository, error) {
 			URL:  r.authProvider.GetRepositoryURL(reponame),
 		})
 		if err != nil {
-			return nil, errors.Wrap(err, "clone repository is failed")
+			return nil, fmt.Errorf("clone repository: %w", err)
 		}
 		spinner.Finish()
 	}
 
 	wt, err := rep.Worktree()
 	if err != nil {
-		return nil, errors.Wrap(err, "get worktree is failed")
+		return nil, fmt.Errorf("get worktree: %w", err)
 	}
 
 	if revision == "" {
 		target, err := r.resolveReference(rep, branch)
 		if err != nil {
-			return nil, errors.Wrapf(err, "change branch to %s is failed", branch)
+			return nil, fmt.Errorf("change branch to %s: %w", branch, err)
 		}
 
 		if err := wt.Checkout(&git.CheckoutOptions{Hash: target.Hash()}); err != nil {
-			return nil, errors.Wrapf(err, "checkout to %s is failed", revision)
+			return nil, fmt.Errorf("checkout revision to %s: %w", revision, err)
 		}
 
 		head := plumbing.NewHashReference(plumbing.HEAD, target.Hash())
 		if err := rep.Storer.SetReference(head); err != nil {
-			return nil, errors.Wrapf(err, "set head to %s is failed", branch)
+			return nil, fmt.Errorf("set head to %s: %w", branch, err)
 		}
 	} else {
 		var opts git.CheckoutOptions
@@ -119,7 +118,7 @@ func (r *github) Open() (*OpenedRepository, error) {
 		tag := plumbing.NewTagReferenceName(revision)
 		_, err := rep.Reference(tag, false)
 		if err != nil && err != plumbing.ErrReferenceNotFound {
-			return nil, errors.Wrapf(err, "tag = %s", tag)
+			return nil, fmt.Errorf("tag '%s' reference: %w", tag, err)
 		} else {
 			if err != nil {
 				// Tag not found, revision must be a hash
@@ -133,18 +132,18 @@ func (r *github) Open() (*OpenedRepository, error) {
 		}
 
 		if err := wt.Checkout(&opts); err != nil {
-			return nil, errors.Wrapf(err, "checkout to %s is failed", revision)
+			return nil, fmt.Errorf( "checkout to %s: %w", revision, err)
 		}
 	}
 
 	commiter, err := rep.Log(&git.LogOptions{})
 	if err != nil {
-		return nil, errors.Wrap(err, "get commit is failed")
+		return nil, fmt.Errorf("get commit: %w", err)
 	}
 
 	current, err := commiter.Next()
 	if err != nil {
-		return nil, errors.Wrap(err, "get commit current is failed")
+		return nil, fmt.Errorf("get current commit: %w", err)
 	}
 
 	return &OpenedRepository{
